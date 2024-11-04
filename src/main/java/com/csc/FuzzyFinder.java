f901496270651d9004d4dcb7aa592481d224a853
jcobcorpuz <cc62200@mail.harpercollege.edu>
2024-11-03 22:52:22 -0600

    add ons

--- src/main/java/com/csc/FuzzyFinder.java
+++ src/main/java/com/csc/FuzzyFinder.java
@@ -3,6 +3,19 @@ package com.csc;
 import java.util.ArrayList;
 
 public class FuzzyFinder {
+
+    /**
+     * Worst-case scenario for linear analysis:
+     * In the worst case for a linear search on a randomized list of fuzzies,
+     * the target element, "gold", is at the last position in the list or not present at all.
+     * The algorithm must check each element once if the target is at the last position or not present at all.
+     * If the list has n elements, the number of checks in the worst case is n.
+     * Mathematical expression: O(n)
+     *
+     * @param fuzzies
+     * @return
+     */
+
     public int linearSearch(ArrayList<Fuzzy> fuzzies){
         for (int i = 0; i < fuzzies.size(); i++){
             if(fuzzies.get(i).color.equals("gold")){
@@ -11,6 +24,20 @@ public class FuzzyFinder {
         }
         return -1;
     }
+
+
+    /**
+     * Worst-case scenario for binary analysis:
+     * In the worst case for a binary search on a randomized list of fuzzies,
+     * the target element, "gold", is either not present or requires the maximum number
+     * of checks to be found. Binary search works by repeatedly halving the search space,
+     * so the number of checks in the worst case is proportional to the logarithm of the list
+     * size. If the list has n elements, the number of checks in the worst case is about log2(n).
+     * Mathematical expression: O(log n)
+     *
+     * @param fuzzies
+     * @return
+     */
 
     public int binarySearch(ArrayList<Fuzzy> fuzzies){
         int left = 0;
